8e32b6b491d9723db0e97f71655163836a99a13d
amrayman100 <amrayman1996@yahoo.com>
2023-05-12 21:35:12 +0300

    health check and integration test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use server::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:9000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
